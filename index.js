ca5c4f1fc3f3b0f775208e2aa511e8220f18ac43
Joshua Cleverley <JoshuaCleverley@protonmail.com>
2022-04-04 09:49:20 +0100

    Sprint 3.0 Added simple content to the home page of the website

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let navMenu;
 let hamburger;
 
+toggleMenu = () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
 window.onload = () => {
     hamburger = document.getElementById("hamburger");
     navMenu = document.getElementById("nav-menu");
 
     hamburger.addEventListener("click", toggleMenu);
-}
-
-toggleMenu = () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
 }
